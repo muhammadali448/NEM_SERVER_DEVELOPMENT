54b339b4b29b150352b3bbedcca30bad50903abd
muhammadali448 <muhammadali19960@outlook.com>
2019-08-15 19:16:04 +0500

    Cors

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var passport = require("passport");
 var authenticate = require("../authenticate");
 const bodyParser = require("body-parser");
+const cors = require("./cors");
 var User = require("../models/user");
 router.use(bodyParser.json());
 
 router.get(
   "/",
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   (req, res, next) => {
@@ -24,7 +26,7 @@ router.get(
   }
 );
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", cors.corsWithOptions, (req, res, next) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -54,18 +56,23 @@ router.post("/signup", (req, res, next) => {
   );
 });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  var token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({
-    success: true,
-    token: token,
-    status: "You are successfully logged in!"
-  });
-});
+router.post(
+  "/login",
+  cors.corsWithOptions,
+  passport.authenticate("local"),
+  (req, res) => {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!"
+    });
+  }
+);
 
-router.get("/logout", (req, res) => {
+router.get("/logout", cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie("session-id");
