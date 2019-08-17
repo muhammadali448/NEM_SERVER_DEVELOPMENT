88dc0eba92263c3db481693b131db06088c35db3
muhammadali448 <muhammadali19960@outlook.com>
2019-08-17 11:46:28 +0500

    new

--- conFusionServer/routes/cors.js
+++ conFusionServer/routes/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 var whitelist = ['http://localhost:3000', 'https://localhost:3443', 
-'http://muhammad:3001']
+'http://localhost:3001']
 var corsOptionsDelegate = (req, callback) => {
   var corsOptions;
   console.log(req.header("Origin"));
